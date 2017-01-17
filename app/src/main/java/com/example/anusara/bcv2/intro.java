bd22bee5ce99b2581e89f6cff217fed5d52898bd
anusaragd <anusara.orm@gmail.com>
2017-01-18 02:31:16 +0700

    BCv2

--- app/src/main/java/com/example/anusara/bcv2/intro.java
+++ app/src/main/java/com/example/anusara/bcv2/intro.java
@@ -1,13 +1,46 @@
 package com.example.anusara.bcv2;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class intro extends AppCompatActivity {
 
+    private Handler handler;
+    private Runnable runnable;
+    long delay_time;
+    long time = 3000L;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            public void run() {
+                Intent intent = new Intent(intro.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
     }
+
+    public void onResume() {
+        super.onResume();
+        delay_time = time;
+        handler.postDelayed(runnable, delay_time);
+        time = System.currentTimeMillis();
+    }
+
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+        time = delay_time - (System.currentTimeMillis() - time);
+    }
+
 }
+
